7fb0aac12129edce17df628e08ebeee41f3e0ec2
lilygomes <44775170+lilygomes@users.noreply.github.com>
2025-11-18 16:52:03 -0500

    i fought the Lab the Lab won...no... Lab 5...

---
+++ Project1/app/src/main/java/com/example/Project1/ItemList.java
@@ -0,0 +1,28 @@
+package com.example.Lab3;
+
+import java.util.LinkedList;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+//////////////////////////////////////////////////////////////////////////
+//
+//  Singleton class ItemList, for creating/managing a single
+//  instance of a list of items (at the moment, strings).  Note
+//  that the type "String" can easily be changed to accommodate a
+//  list of whatever kind of objects you wish.
+//
+//  Author: M. Halper
+//
+//////////////////////////////////////////////////////////////////////////
+
+@Singleton
+public class ItemList extends LinkedList<String>
+{
+    @Inject
+    ItemList()
+    {
+        // default constructor
+    }
+
+} // end ItemList
